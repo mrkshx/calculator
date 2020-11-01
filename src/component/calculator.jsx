0443a797a7300698a48dcf50d28fc4dc7ac7dc9d
Markus Mengel <markus.mengelhx@gmail.com>
2020-11-01 17:25:20 +0100

    created empty entry_pad component

--- src/component/calculator.jsx
+++ src/component/calculator.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Display from './display';
+import EntryPad from './entry_pad';
 
 import "../style/calculator.scss";
 
 const Calculator = () => {
   return <div id="calculator">
     <Display/>
+    <EntryPad/>
   </div>
 }
 
---
+++ src/component/entry_pad.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const EntryPad = () => {
+
+  return <div id="entry_pad" >
+  </div>
+}
+
+
+export default EntryPad;
