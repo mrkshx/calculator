831df10764cc3d5ce719802e9d5d38bfced74594
Markus Mengel <markus.mengelhx@gmail.com>
2020-11-01 16:14:55 +0100

    created empty display component

--- src/component/calculator.jsx
+++ src/component/calculator.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Display from './display';
+
 import "../style/calculator.scss";
 
 const Calculator = () => {
   return <div id="calculator">
+    <Display/>
   </div>
 }
 
