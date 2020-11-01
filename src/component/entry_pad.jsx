a2cdd7d1ddd2c88b2d092f7b19c28be19fa8b2b1
Markus Mengel <markus.mengelhx@gmail.com>
2020-11-01 18:28:29 +0100

    added key component to entry pad for testing

--- src/component/entry_pad.jsx
+++ src/component/entry_pad.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Key from './key';
+
 import "../style/entry_pad.scss";
 
 const EntryPad = () => {
 
   return <div id="entry_pad" >
+    <Key/>
   </div>
 }
 
