99384e3771296833a7142b15c23a82c597259595
Markus Mengel <markus.mengelhx@gmail.com>
2020-11-01 23:17:31 +0100

    added numbers Key to NumberPad

--- src/component/number_pad.jsx
+++ src/component/number_pad.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import Key from './key';
 
 const NumberPad = () => {
 
   return <div id="clear_pad" >
+    <Key value="7" type="number" />
+    <Key value="8" type="number" />
+    <Key value="9" type="number" />
+    <Key value="4" type="number" />
+    <Key value="5" type="number" />
+    <Key value="6" type="number" />
+    <Key value="1" type="number" />
+    <Key value="2" type="number" />
+    <Key value="3" type="number" />
+    <Key value="0" type="number" />
+    <Key value="." type="number" />
   </div>
 }
 
