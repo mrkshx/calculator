32195052fd32d1c34548fe6a1ba22c69e72d88bf
Markus Mengel <markus.mengelhx@gmail.com>
2020-11-02 12:02:50 +0100

    changed alignment for clear keys

--- src/component/key.jsx
+++ src/component/key.jsx
@@ -4,9 +4,23 @@ import "../style/key.scss";
 
 const Key = (props) => {
 
+  const setKeyId = () => {
+    let result;
+    if (props.type === "number") {
+      if (props.value === ".") {
+        result = "numdot"
+      }
+      else result = `num${props.value}`
+    }
+    else if (props.type === "clear") {
+      result = `clear${props.value}`
+    }
+    return result;
+  }
+
   return <div
     className={`key ${props.type}_key`}
-    id={props.type === "number" ? props.value === "." ? "numdot" : `num${props.value}` : "" }>
+    id={setKeyId(props)}>
     <p>{props.value}</p>
   </div>
 }
