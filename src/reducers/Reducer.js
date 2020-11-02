59370c7fecc6788ff79b4a3ec28748998cebe95a
Markus Mengel <markus.mengelhx@gmail.com>
2020-11-02 15:30:29 +0100

    created reducer

---
+++ src/reducers/Reducer.js
@@ -0,0 +1,24 @@
+const Reducer= (state, action) => {
+
+  switch(action.type){
+    case 'INPUT_KEYS':
+      return{
+        ...state,
+        output: action.payload
+      };
+    case 'RESET_DISPLAY':
+      return{
+        ...state,
+        output: "0"
+      };
+    case 'CALCULATE':
+      return{
+        ...state,
+        output: action.payload
+      };
+    default: return state;
+  };
+};
+
+
+export default Reducer;
