f7c8bf8c535be91b83873cdac6bcdbb278624aef
Markus Mengel <markus.mengelhx@gmail.com>
2020-11-02 01:24:27 +0100

    added operator Keys to OperatorPad

--- src/component/operator_pad.jsx
+++ src/component/operator_pad.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import Key from './key';
+
 import "../style/operator_pad.scss";
 
 
 const OperatorPad = () => {
 
   return <div id="operator_pad" >
+    <Key value=":" type="operator" />
+    <Key value="x" type="operator" />
+    <Key value="+" type="operator" />
+    <Key value="-" type="operator" />
+    <Key value="=" type="operator" />
   </div>
 }
 
